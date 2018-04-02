910f2bce292b94e4451d454510b3f487a1e74926
boser87 <stefano.celentano87@gmail.com>
2018-04-02 22:39:57 +0200

    changed repository code to insert ride using SimpleJdbcInsert

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -30,7 +33,29 @@ public class RideRepositoryImpl implements RideRepository {
 	    // update is used for insert update and delete
         // uses the standard prepared statement approach
 
-		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());
+		//jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());
+
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+
+		simpleJdbcInsert.setTableName("ride");
+
+		simpleJdbcInsert.setColumnNames(columns);
+
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", ride.getName());
+		data.put("duration", ride.getDuration());
+
+		// in this case we can return the key that was created automatically from the db
+
+		simpleJdbcInsert.setGeneratedKeyName("id");
+
+		Number key = simpleJdbcInsert.executeAndReturnKey(data);
+
+		System.out.println(key);
 
 		return null;
 	}
